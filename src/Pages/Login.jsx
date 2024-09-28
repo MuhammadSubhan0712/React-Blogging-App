96650cef822d58f316765b8bb873c412f1852de9
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-09-29 04:37:05 +0500

    My blogging App

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -7,27 +7,6 @@ const Login = () => {
 
   
   return (
-
-    <div className='bg-base-300 flex flex-col h-screen'> 
-        
-    <div className=" bg-primary text-white w-full h-10 p-2 flex justify-around">
-      <div className="">
-        <a className="normal-case text-xl font-bold"
-          >Personal Blogging App</a>
-
-      </div>
-      <div className="text-xl font-bold">
-       <button><a href="./register.html">SignUp</a></button>
-      </div> 
-    </div>
- 
-        <h1 className="text-4xl flex justify-center font-bold bg-white w-full p-2  mt-5">
-            Login
-        </h1>
-
-    {/*  --------------------------------------------------------------- }
-
-    {/* login  Form  */}
   
     <div className="flex-grow flex items-center justify-center w-full">
       <div className="w-full max-w-xs">
