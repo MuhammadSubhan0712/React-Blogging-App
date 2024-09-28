909720c9e6e56abb6431e48d30666cc893472f51
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-09-29 04:07:23 +0500

    Firebase Config

---
+++ src/Config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBzk6TSHdMJ3uJVSDBiaDRN7qNphremi5M",
+  authDomain: "react-blog-app-ca446.firebaseapp.com",
+  projectId: "react-blog-app-ca446",
+  storageBucket: "react-blog-app-ca446.appspot.com",
+  messagingSenderId: "682736785998",
+  appId: "1:682736785998:web:807f8fb5ff6627e8fc811f",
+  measurementId: "G-J5B0S8NHLJ",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
